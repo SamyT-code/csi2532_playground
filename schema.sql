fd81d74db67b169badad14be5cae1f557db22743
Samy Touabi <stoua026@uottawa.ca>
2022-01-24 23:18:07 -0500

    lab2 branch

---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE voitures(
+  marque varchar(255),
+  anee integer,
+  model varchar(255)
+);
---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO voitures
+VALUES('Toyota', 2002, 'Corolla'),
+      ('Nissan', 2018, 'Pathfinder'),
+      ('Tesla', 2020, 'Model X'),
+      ('WV', 1933, 'Moustache'),
+      ('Ferarri', 1990, 'Arrogance');
